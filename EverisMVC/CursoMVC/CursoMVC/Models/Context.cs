6cc3feaa8a31e36ae4e78810008e64dff14998d3
antonioliverjr <antoniobatistajr@gmail.com>
2021-10-17 15:40:19 -0300

    Desenvolvimento de aplicações com .NET bootcamp Everis

---
+++ EverisMVC/CursoMVC/CursoMVC/Models/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public virtual DbSet<Categoria> Categorias { get; set; }
+        public virtual DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DELLJR\SQLEXPRESS;Database=Cursomvc;User ID=project;Password=Jrdbsql");
+        }
+
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ EverisMVC/CursoMVC/CursoMVC/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Informe uma descrição!")]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = "Digite a quantidade!")]
+        [Range(1, 10, ErrorMessage = "Digite uma quantidade entre 1 e 10!")]
+        public int Quantidade { get; set; }
+        [Display(Name = "Categoria")]
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+        
+    }
+}
