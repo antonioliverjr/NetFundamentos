f624569858f523c45fad9ba64a279883ef05dd75
antonioliverjr <antoniobatistajr@gmail.com>
2021-12-05 15:17:12 -0300

    Treinando macros em C#

---
+++ Diversos/Selenium_Linkedin/Auto_Linkedin/Auto_Linkedin/Program.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Auto_Linkedin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver seleniumSite = new ChromeDriver("C:\\");
+
+            seleniumSite.Url = "https://www.selenium.dev/pt-br/documentation/webdriver/";
+
+            Thread.Sleep(5000);
+
+            seleniumSite.SwitchTo().NewWindow(WindowType.Tab);
+
+            LinkedinAcesso(seleniumSite);
+        }
+
+        public static void LinkedinAcesso(IWebDriver seleniumSite)
+        {
+            //IWebDriver driver = new ChromeDriver("C:\\");
+
+            IWebDriver driver = seleniumSite;
+
+            driver.Url = "https://www.linkedin.com/login";
+
+            driver.FindElement(By.Id("username")).SendKeys("antoniobatistajr@gmail.com");
+
+            driver.FindElement(By.Id("password")).SendKeys("" + Keys.Enter);
+
+            driver.FindElement(By.XPath("//input[@placeholder='Pesquisar']")).SendKeys("Developer Backend Net" + Keys.Return);
+
+            Thread.Sleep(5000);
+
+            driver.FindElement(By.XPath("//button[@aria-label='Vagas']")).Click();
+        }
+    }
+}
---
+++ Diversos/Send_Whatsapp/Whatsapp/Whatsapp/Program.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace Whatsapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string telDestino;
+            string urlSend = "https://api.whatsapp.com/";
+
+            
+
+            Console.WriteLine("Informe o número de telefone para envio no formato 5571999999999: ");
+            
+            telDestino = "send?phone="+Console.ReadLine();
+
+            string textoEnvio = "&text="+@"Favor não responda essa mensagem, tentando enviar msg automática";
+
+            urlSend = urlSend + telDestino + textoEnvio;
+
+            IWebDriver whatsappSend = new ChromeDriver("C:\\");
+
+            whatsappSend.Url = urlSend;
+
+            Thread.Sleep(5000);
+
+            whatsappSend.FindElement(By.Id("action-button")).Click();
+
+            Thread.Sleep(3000);
+
+            whatsappSend.FindElement(By.XPath("//*[@id='fallback_block']/div/div/a")).Click();
+            
+            Thread.Sleep(10000);
+
+            whatsappSend.FindElement(By.XPath("//span[@data-icon='send']")).Click();
+
+            // Thread.Sleep(50000);
+
+            //whatsappSend.Close();
+
+
+        }
+    }
+}
